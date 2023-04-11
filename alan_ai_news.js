9215d768af6e22eb58ea8f1274bbd214746ff157
amandekate <56401874+amandekate@users.noreply.github.com>
2023-04-12 01:38:11 +0530

    fix

--- alan_ai_news.js
+++ alan_ai_news.js
@@ -7,28 +7,27 @@ let savedArticles = [];
 // News by Source
 intent('Give me the news from $(source* (.*))', (p) => {
     let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
-
+    
     if(p.source.value) {
         NEWS_API_URL = `${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`
     }
+     
+ 
+   api.request(NEWS_API_URL, {headers: {"user-agent": 'user agent' }}, (error, response, body) => {
+    const { totalResults, articles } = JSON.parse(body);
 
-    api.request(NEWS_API_URL, (error, response, body) => {
-        const { articles } = JSON.parse(body);
-
-        if(!articles.length) {
-            p.play('Sorry, please try searching for news from a different source');
-            return;
-        }
-
+    if(totalResults == 0) {
+        p.play('Sorry, please try searching for news from a different source');
+        return;
+    }
+        
         savedArticles = articles;
-
+        
         p.play({ command: 'newHeadlines', articles });
         p.play(`Here are the (latest|recent) ${p.source.value}.`);
-
-        p.play('Would you like me to read the headlines?');
-        p.then(confirmation);
+   
     });
-})
+});
 
 // News by Term
 intent('what\'s up with $(term* (.*))', (p) => {
